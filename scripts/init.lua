69e10e2cff3fcebb1d725d2b1693c126a209b010
MadagascarDave <dave.mada@gmail.com>
2021-05-21 06:41:08 -0600

    Initial code 2.4.1.0 of the seasons tracker by Herreteman

---
+++ scripts/init.lua
@@ -0,0 +1,34 @@
+-- Load scripts required by this pack
+ScriptHost:LoadScript("scripts/access.lua")
+ScriptHost:LoadScript("scripts/area.lua")
+
+-- Load items
+-- Hosted items don't work without this??
+Tracker:AddItems("items.json")
+
+-- Load maps
+Tracker:AddMaps("maps/maps.json")
+
+if Tracker.ActiveVariantUID == "dungeon_shuffle_hard" then
+	Tracker:AddLocations("maps/hard/holodrum.json")
+	Tracker:AddLocations("maps/hard/subrosia.json")
+	Tracker:AddLocations("maps/dungeon_shuffle/hard.json")
+end
+
+if Tracker.ActiveVariantUID == "dungeon_shuffle" then
+	Tracker:AddLocations("maps/normal/holodrum.json")
+	Tracker:AddLocations("maps/normal/subrosia.json")
+	Tracker:AddLocations("maps/dungeon_shuffle/normal.json")
+end
+
+if Tracker.ActiveVariantUID == "hard" then
+	Tracker:AddLocations("maps/hard/holodrum.json")
+	Tracker:AddLocations("maps/hard/subrosia.json")
+	Tracker:AddLocations("maps/hard/dungeons.json")
+end 
+
+if Tracker.ActiveVariantUID == "standard" then
+	Tracker:AddLocations("maps/normal/holodrum.json")
+	Tracker:AddLocations("maps/normal/subrosia.json")
+	Tracker:AddLocations("maps/normal/dungeons.json")
+end
